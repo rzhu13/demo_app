5d5adb5cec4767fc4dd11c468783183439eb907f
Raymond Zhu <raymond.zhu@yale.edu>
2013-02-19 00:22:17 -0500

    Fixed app/models/micropost.rb

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,8 +1,8 @@
 class Micropost < ActiveRecord::Base
   attr_accessible :Content, :User_ID
 
-	belongs_to :User
+  belongs_to :User
 
-  validates :content, :length => { :maximum => 140 }
+  validates :Content, :length => { :maximum => 140 }
 
 end
